5867479df9ab99ea2f6f8fb64d48217fafefec6b
hafijul233 <hafijul233@users.noreply.github.com>
2024-08-12 18:39:42 +0000

    Fix styling

--- src/Http/Controllers/Reload/WalletToPrepaidCardController.php
+++ src/Http/Controllers/Reload/WalletToPrepaidCardController.php
@@ -6,7 +6,6 @@ use Exception;
 use Fintech\Auth\Facades\Auth;
 use Fintech\Core\Enums\Auth\RiskProfile;
 use Fintech\Core\Enums\Auth\SystemRole;
-use Fintech\Core\Enums\Reload\DepositStatus;
 use Fintech\Core\Enums\Transaction\OrderStatus;
 use Fintech\Core\Enums\Transaction\OrderStatusConfig;
 use Fintech\Core\Exceptions\DeleteOperationException;
