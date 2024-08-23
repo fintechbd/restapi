4561ae8028f951b7be498a4772d6a406a5872112
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-24 02:26:33 +0600

    chart controller moved

---
+++ src/Http/Controllers/Business/Charts/ServiceRateCostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fintech\RestApi\Http\Controllers\Business\Charts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServiceRateCostController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
---
+++ src/Http/Controllers/Reload/Charts/DepositPartnerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fintech\RestApi\Http\Controllers\Reload\Charts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DepositPartnerController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
