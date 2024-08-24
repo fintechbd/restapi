073ae8f50c67b7809becbdc2cf8bc149a6309174
hafijul233 <hafijul233@users.noreply.github.com>
2024-08-24 08:48:27 +0000

    Fix styling

--- src/Http/Requests/Auth/PasswordResetRequest.php
+++ src/Http/Requests/Auth/PasswordResetRequest.php
@@ -24,8 +24,7 @@ class PasswordResetRequest extends FormRequest
     {
         return [
             'token' => ['required', 'string'],
-            config('fintech.auth.password_field', 'password')
-            => [...config('fintech.auth.password_field_rules', ['required', 'string', 'min:8']), 'confirmed'],
+            config('fintech.auth.password_field', 'password') => [...config('fintech.auth.password_field_rules', ['required', 'string', 'min:8']), 'confirmed'],
         ];
     }
 }
