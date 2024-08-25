7ac0c2589a3bf113e22c9db30ca100cf477421ff
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-25 13:00:59 +0600

    Transaction summary added

--- src/Http/Controllers/Transaction/Charts/OrderSummaryController.php
+++ src/Http/Controllers/Transaction/Charts/OrderSummaryController.php
@@ -3,6 +3,7 @@
 namespace Fintech\RestApi\Http\Controllers\Transaction\Charts;
 
 use App\Http\Controllers\Controller;
+use Fintech\RestApi\Http\Resources\Transaction\Charts\OrderSummaryCollection;
 use Illuminate\Http\Request;
 
 class OrderSummaryController extends Controller
@@ -12,6 +13,12 @@ class OrderSummaryController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $orders = collect([
+            ['Transaction Type' => 'Bank Transfer', 'No of Transactions' => '65', 'Total Amount (CAD)' => '35700'],
+            ['Transaction Type' => 'Cash Pickup', 'No of Transactions' => '15', 'Total Amount (CAD)' => '12050'],
+            ['Transaction Type' => 'Wallet', 'No of Transactions' => '29', 'Total Amount (CAD)' => '2100'],
+        ]);
+
+        return new OrderSummaryCollection($orders);
     }
 }
