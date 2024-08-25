11454c65bada76d58ba02e466110392ca8fac518
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-25 12:28:03 +0600

    deposit payin api mock added

--- src/Http/Controllers/Reload/Charts/DepositPartnerController.php
+++ src/Http/Controllers/Reload/Charts/DepositPartnerController.php
@@ -3,6 +3,7 @@
 namespace Fintech\RestApi\Http\Controllers\Reload\Charts;
 
 use App\Http\Controllers\Controller;
+use Fintech\RestApi\Http\Resources\Reload\Charts\DepositPartnerCollection;
 use Illuminate\Http\Request;
 
 class DepositPartnerController extends Controller
@@ -12,9 +13,13 @@ class DepositPartnerController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return response()->success([
-            'data' => [],
-            'query' => $request->all(),
+        $collection = collect([
+            ['Mode' => 'Bank Account', 'Bank Name' => 'RBC', 'Bank A/C No' => '15787477', 'Limits (CAD)' => '1000', 'Fees/Charges' => '2.50'],
+            ['Mode' => 'Bank Account', 'Bank Name' => 'CIBC', 'Bank A/C No' => '9807979991', 'Limits (CAD)' => '1500', 'Fees/Charges' => '2.00'],
+            ['Mode' => 'Card', 'Bank Name' => 'Credit Card', 'Bank A/C No' => '........895', 'Limits (CAD)' => '500', 'Fees/Charges' => '1.20%'],
+            ['Mode' => 'Paypal', 'Bank Name' => 'Paypal', 'Bank A/C No' => '1223341334', 'Limits (CAD)' => '2000', 'Fees/Charges' => '0.8%'],
+            ['Mode' => 'Paypal', 'Bank Name' => 'Paypal', 'Bank A/C No' => '1223341334', 'Limits (CAD)' => '2000', 'Fees/Charges' => '0.8%'],
         ]);
+        return new DepositPartnerCollection($collection);
     }
 }
