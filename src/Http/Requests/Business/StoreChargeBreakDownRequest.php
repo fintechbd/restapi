32209688eee6eb2f93390b2d1056e3fe0b35b0e4
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-02 17:59:44 +0600

    charge break down limit rule added

--- src/Http/Requests/Business/StoreChargeBreakDownRequest.php
+++ src/Http/Requests/Business/StoreChargeBreakDownRequest.php
@@ -2,6 +2,9 @@
 
 namespace Fintech\RestApi\Http\Requests\Business;
 
+use Fintech\Business\Facades\Business;
+use Fintech\Core\Rules\ChargeHigherLimit;
+use Fintech\Core\Rules\ChargeLowerLimit;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -22,11 +25,16 @@ class StoreChargeBreakDownRequest extends FormRequest
      */
     public function rules(): array
     {
+        $serviceStat = Business::serviceStat()->find($this->input(['service_stat_id']));
+
+        $stat_lower_limit = floatval($serviceStat->service_stat_data[0]['lower_limit']);
+        $stat_higher_limit = floatval($serviceStat->service_stat_data[0]['higher_limit']);
+
         return [
             'service_stat_id' => ['integer', 'required'],
             'service_id' => ['integer', 'required'],
-            'lower_limit' => ['numeric', 'required'],
-            'higher_limit' => ['numeric', 'required'],
+            'lower_limit' => ['numeric', 'required', "gte:{$stat_lower_limit}", "lte:{$stat_higher_limit}", new ChargeLowerLimit()],
+            'higher_limit' => ['numeric', 'required', "gte:{$stat_lower_limit}", "lte:{$stat_higher_limit}", new ChargeHigherLimit()],
             'charge' => ['string', 'required'],
             'discount' => ['string', 'required'],
             'commission' => ['string', 'required'],
