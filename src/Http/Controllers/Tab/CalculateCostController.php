f35fef236ce766621b4fe1b8a514d2ed25f3edf5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-04 20:17:13 +0600

    dump updated

--- src/Http/Controllers/Tab/CalculateCostController.php
+++ src/Http/Controllers/Tab/CalculateCostController.php
@@ -59,35 +59,8 @@ class CalculateCostController extends Controller
             $quoteInfo = Tab::assignVendor()->requestQuote($quote);
 
             $inputs['amount'] = 100;
-            $exchangeRate = Business::serviceStat()->cost($inputs);
 
-            $exchangeRate = json_decode(
-                <<<JSON
-                {
-                    "rate": 85.66996,
-                    "input": "CAD",
-                    "output": "BDT",
-                    "input_unit": "CAD 1.00",
-                    "output_unit": "BDT 85.67",
-                    "input_symbol": "$",
-                    "output_symbol": "\u09f3",
-                    "amount": "20",
-                    "amount_formatted": "CAD 20.00",
-                    "converted": "1713.4",
-                    "converted_formatted": "BDT 1,713.40",
-                    "charge": "4%",
-                    "charge_amount": "0.8",
-                    "discount": "6%",
-                    "discount_amount": "1.2",
-                    "commission": "0",
-                    "commission_amount": "0",
-                    "total_amount": "19.6",
-                    "charge_amount_formatted": "CAD 0.80",
-                    "discount_amount_formatted": "CAD 1.20",
-                    "commission_amount_formatted": "CAD 0.00",
-                    "total_amount_formatted": "CAD 19.60"
-                }
-           JSON, true);
+            $exchangeRate = Business::serviceStat()->cost($inputs);
 
             $exchangeRate['vendor_info'] = $quoteInfo;
 
