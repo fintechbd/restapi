c34b00f8ee02f1a44af60316553f316a1afd7e50
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-09 13:55:04 +0600

    service stat store and update issue fixed

--- src/Http/Requests/Business/StoreServiceStatRequest.php
+++ src/Http/Requests/Business/StoreServiceStatRequest.php
@@ -52,7 +52,7 @@ class StoreServiceStatRequest extends FormRequest
             'enabled' => true,
             'paginate' => false])->each(function ($item) use (&$rules) {
                 if (! isset($rules['service_stat_data.'.$item->service_setting_field_name])) {
-                    $rules[$rules['service_stat_data.'.$item->service_setting_field_name]] = $item->service_setting_rule ?? 'nullable';
+                    $rules['service_stat_data.'.$item->service_setting_field_name] = $item->service_setting_rule ?? 'nullable';
                 }
             });
 
--- src/Http/Requests/Business/UpdateServiceStatRequest.php
+++ src/Http/Requests/Business/UpdateServiceStatRequest.php
@@ -35,10 +35,11 @@ class UpdateServiceStatRequest extends FormRequest
             'service_stat_data' => ['array', 'required'],
             'service_stat_data.lower_limit' => ['string', 'required', 'numeric'],
             'service_stat_data.higher_limit' => ['string', 'required', 'numeric'],
-            'service_stat_data.local_currency_higher_limit' => ['string', 'required', 'numeric'],
+            'service_stat_data.local_currency_lower_limit' => ['string', 'nullable', 'numeric'],
+            'service_stat_data.local_currency_higher_limit' => ['string', 'nullable', 'numeric'],
             'service_stat_data.charge' => ['string', 'required', new PercentNumber],
             'service_stat_data.discount' => ['string', 'required', new PercentNumber],
-            'service_stat_data.commission' => ['string', 'required', new PercentNumber],
+            'service_stat_data.commission' => ['string', 'nullable', new PercentNumber],
             'service_stat_data.cost' => ['string', 'required', new PercentNumber],
             'service_stat_data.charge_refund' => ['string', 'required', 'in:yes,no'],
             'service_stat_data.discount_refund' => ['string', 'required', 'in:yes,no'],
@@ -50,7 +51,7 @@ class UpdateServiceStatRequest extends FormRequest
             'enabled' => true,
             'paginate' => false])->each(function ($item) use (&$rules) {
                 if (! isset($rules['service_stat_data.'.$item->service_setting_field_name])) {
-                    $rules[$rules['service_stat_data.'.$item->service_setting_field_name]] = $item->service_setting_rule ?? 'nullable';
+                    $rules['service_stat_data.'.$item->service_setting_field_name] = $item->service_setting_rule ?? 'nullable';
                 }
             });
 
