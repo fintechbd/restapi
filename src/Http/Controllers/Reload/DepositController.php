ea95a467aa20f0b623c88ee6dc8064725ba50347
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-15 23:31:15 +0600

    console layout updated

--- src/Http/Controllers/Reload/DepositController.php
+++ src/Http/Controllers/Reload/DepositController.php
@@ -55,12 +55,8 @@ class DepositController extends Controller
     {
         try {
             $inputs = $request->validated();
-            //$inputs['transaction_form_id'] = Transaction::transactionForm()->list(['code' => 'point_reload'])->first()->getKey();
             $inputs['transaction_form_code'] = 'point_reload';
-            //$inputs['service_id'] = Business::serviceType()->list(['service_type_slug'=>'bangladesh_top_up']);
-            //$inputs['service_type_slug'] = 'fund_deposit';
             $depositPaginate = Reload::deposit()->list($inputs);
-            //            $depositPaginate = Transaction::order()->list($inputs);
 
             return new DepositCollection($depositPaginate);
 
