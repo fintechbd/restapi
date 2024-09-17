6aed435812843af317bc9907a4fd4343d2cb519a
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-17 14:17:08 +0600

    ipapi response structured

--- src/Http/Resources/Auth/PulseCheckResource.php
+++ src/Http/Resources/Auth/PulseCheckResource.php
@@ -2,6 +2,7 @@
 
 namespace Fintech\RestApi\Http\Resources\Auth;
 
+use Fintech\MetaData\Facades\MetaData;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,7 +15,7 @@ class PulseCheckResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return match (config('fintech.auth.geoip.default')) {
+        $data = match (config('fintech.auth.geoip.default')) {
             'ipapi' => $this->ipapi($request),
             'ipinfo' => $this->ipinfo($request),
             'ipdata' => $this->ipdata($request),
@@ -24,48 +25,63 @@ class PulseCheckResource extends JsonResource
             'maxmind' => $this->maxmind($request),
             default => $this->local($request)
         };
+
+        $data['location']['country']['id'] = $data['country']?->getKey() ?? null;
+        $data['location']['country']['name'] = $data['country']?->name ?? null;
+        $data['location']['country']['code'] = $data['country']?->iso2 ?? null;
+        $data['location']['country']['logo_png'] = $data['country']?->getFirstMedia('logo_png') ?? null;
+        $data['location']['country']['logo_svg'] = $data['country']?->getFirstMedia('logo_svg') ?? null;
+
+        $data['location']['state']['id'] = $data['state']?->getKey() ?? null;
+        $data['location']['state']['type'] = $data['state']?->type ?? null;
+        $data['location']['state']['name'] = $data['state']?->name ?? null;
+
+        $data['location']['city']['id'] = $data['city']?->getKey() ?? null;
+        $data['location']['city']['type'] = $data['city']?->type ?? null;
+        $data['location']['city']['name'] = $data['city']?->name ?? null;
+
+        $data['location']['timezones'] = $data['country']?->timezones ?? [];
+        $data['location']['language'] = $data['country']?->language ?? [];
+        $data['location']['calling_code'] = '+' . str_replace(['+', '-'], '', ($data['country']?->phone_code ?? ''));
+
+        $data['currency']['id'] = $data['country']?->getKey() ?? null;
+        $data['currency']['code'] = $data['country']?->currency ?? null;
+        $data['currency']['name'] = $data['country']?->currency_name ?? null;
+        $data['currency']['symbol'] = $data['country']?->currency_symbol ?? null;
+
+        $data['security']['is_proxy'] = null;
+        $data['security']['proxy_type'] = null;
+        $data['security']['proxy_last_detected'] = null;
+        $data['security']['proxy_level'] = null;
+        $data['security']['is_crawler'] = false;
+        $data['security']['crawler_name'] = null;
+        $data['security']['crawler_type'] = null;
+        $data['security']['is_tor'] = false;
+        $data['security']['threat_level'] = "low";
+        $data['security']['threat_types'] = null;
+        $data['security']['vpn_service'] = null;
+        $data['security']['anonymizer_status'] = null;
+        $data['security']['hosting_facility'] = false;
+
+        unset($data['country'], $data['state'], $data['city']);
+
+        return $data;
     }
 
     private function ipapi(Request $request): array
     {
+        $country = MetaData::country()->list(['iso2' => $this->country_code])->first();
+        $state = MetaData::state()->list(['country_id' => $country->id, 'search' => $this->region_name])->first();
+
         return [
             'ip' => $this->ip ?? null,
             'type' => $this->type ?? 'ipv4',
-            'continent_code' => $this->continent_code ?? null,
-            'continent_name' => $this->continent_name ?? null,
-            'country_code' => $this->country_code ?? null,
-            'country_name' => $this->country_name ?? null,
-            'region_code' => $this->region_code ?? null,
-            'region_name' => $this->region_name ?? null,
+            'country' => $country ?? null,
+            'state' => $state ?? null,
             'city' => $this->city ?? null,
             'zip' => $this->zip ?? null,
             'latitude' => $this->latitude ?? 0,
             'longitude' => $this->longitude ?? 0,
-            'location' => [
-                'capital' => $this->location['capital'] ?? null,
-                'languages' => [
-                    [
-                        'code' => $this->location['languages'][0]['code'] ?? null,
-                        'name' => $this->location['languages'][0]['name'] ?? null,
-                        'native' => $this->location['languages'][0]['native'] ?? null,
-                    ],
-                ],
-                'country_flag' => $this->location['country_flag'] ?? '#',
-                'country_flag_emoji' => $this->location['country_flag_emoji'] ?? '#',
-                'country_flag_emoji_unicode' => $this->location['country_flag_emoji_unicode'] ?? '#',
-                'calling_code' => '+'.str_replace(['+', '-'], '', ($this->location['calling_code'] ?? '')),
-                'is_eu' => $this->location['is_eu'] ?? false,
-            ],
-            'time_zone' => [
-                'id' => $this->time_zone['id'] ?? '',
-                'current_time' => $this->time_zone['current_time'] ?? '',
-                'gmt_offset' => $this->time_zone[''] ?? 0,
-                'code' => $this->time_zone['code'] ?? '0',
-                'is_daylight_saving' => $this->time_zone['is_daylight_saving'] ?? false,
-            ],
-            'currency' => [
-                'code' => $this->currency['code'] ?? 'BDT',
-            ],
         ];
     }
 
