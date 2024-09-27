76e3f20f4c02044feccd3f5250310a926d1a4b57
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-27 18:11:23 +0600

    service type logic updated

--- src/Http/Resources/Business/ServiceTypeListCollection.php
+++ src/Http/Resources/Business/ServiceTypeListCollection.php
@@ -10,55 +10,26 @@ class ServiceTypeListCollection extends ResourceCollection
 {
     private array $serviceTypeList = [];
 
+    private array $defaultServiceSettings = [];
+
     /**
      * Transform the resource collection into an array.
      */
     public function toArray(Request $request): array
     {
-        if (! cache()->has('fintech.serviceTypeList')) {
-            Business::serviceType()->list([
-                'get' => [
-                    'service_types.id',
-                    'service_types.service_type_name',
-                    'service_types.service_type_parent_id',
-                    'service_types.service_type_slug',
-                ],
-                'paginate' => false,
-                'sort' => 'service_types.id',
-            ])
-                ->each(fn ($serviceType) => $this->serviceTypeList[$serviceType->id] = $serviceType->toArray());
-            cache()->put('fintech.serviceTypeList', $this->serviceTypeList, HOUR);
-        } else {
-            $this->serviceTypeList = cache()->get('fintech.serviceTypeList', []);
-        }
+        $this->prepareServiceTypeMetaData();
 
-        Business::serviceSetting()
-            ->list(['enabled' => true, 'paginate' => false, 'service_setting_type' => 'service'])
-            ->each(function ($item) use (&$settings) {
-                $settings[$item->service_setting_field_name] = $item->service_setting_value ?? null;
-                if (is_null($settings[$item->service_setting_field_name])) {
-                    if ($item->service_setting_type_field == 'text') {
-                        $settings[$item->service_setting_field_name] = '';
-                    }
-                }
-            });
-
-        $services = $this->collection->map(function ($item) use ($settings) {
-
+        return $this->collection->map(function ($item) {
             $entries = [];
-
             $this->loadServiceTypeParentList($entries, $item->service_type_parent_id);
-
             $parent = [
                 'id' => null,
                 'service_type_name' => '',
                 'service_type_slug' => '',
             ];
-
             if ($item->service_type_parent_id != null && isset($this->serviceTypeList[$item->service_type_parent_id])) {
                 $parent = $this->serviceTypeList[$item->service_type_parent_id];
             }
-
             return [
                 'id' => $item->id,
                 'logo_svg' => $item->logo_svg,
@@ -73,16 +44,68 @@ class ServiceTypeListCollection extends ResourceCollection
                 'service_id' => $item->service_id ?? null,
                 'service_name' => $item->service_name ?? '',
                 'service_slug' => $item->service_slug ?? '',
-                'service_data' => array_merge($settings, ($item->service_data ?? [])),
+                'service_data' => $this->configureServiceSettingData($item->service_data),
                 'service_vendor_id' => $item->service_vendor_id ?? null,
                 'service_vendor_name' => $item->service_vendor_name ?? '',
                 'destination_country_id' => $item->destination_country_id ?? null,
                 'source_country_id' => $item->source_country_id ?? null,
                 'menu_position' => $item->service_serial ?? -1,
             ];
-        });
+        })->toArray();
+    }
 
-        return $services->toArray();
+    private function prepareServiceTypeMetaData(): void
+    {
+        if (!cache()->has('fintech.serviceTypeList')) {
+            Business::serviceType()->list([
+                'get' => [
+                    'service_types.id',
+                    'service_types.service_type_name',
+                    'service_types.service_type_parent_id',
+                    'service_types.service_type_slug',
+                ],
+                'paginate' => false,
+                'sort' => 'service_types.id',
+            ])
+                ->each(fn($serviceType) => $this->serviceTypeList[$serviceType->id] = $serviceType->toArray());
+            cache()->put('fintech.serviceTypeList', $this->serviceTypeList, HOUR);
+        } else {
+            $this->serviceTypeList = cache()->get('fintech.serviceTypeList', []);
+        }
+
+        Business::serviceSetting()
+            ->list(['enabled' => true, 'paginate' => false, 'service_setting_type' => 'service'])
+            ->each(function ($item) {
+                $this->defaultServiceSettings[$item->service_setting_field_name] = $item->service_setting_value ?? null;
+                if (empty($this->defaultServiceSettings[$item->service_setting_field_name])) {
+                    $this->defaultServiceSettings[$item->service_setting_field_name] = match ($item->service_setting_type_field) {
+                        'text' => '',
+                        default => null
+                    };
+                } else {
+                    $this->defaultServiceSettings[$item->service_setting_field_name] = match ($item->service_setting_type_field) {
+                        'text' => (string)$this->defaultServiceSettings[$item->service_setting_field_name],
+                        'number' => (int)$this->defaultServiceSettings[$item->service_setting_field_name],
+                        default => $this->defaultServiceSettings[$item->service_setting_field_name]
+                    };
+                }
+            });
+    }
+
+    private function configureServiceSettingData($settings): array
+    {
+        if (empty($settings)) {
+            $settings = [];
+        }
+
+        $settings = array_merge($this->defaultServiceSettings, $settings);
+
+        return array_map(function ($entry) {
+            return match ($entry) {
+                'beneficiary_type_id' => empty($entry) ? null : (int)$entry,
+                default => $entry
+            };
+        }, $settings);
     }
 
     private function loadServiceTypeParentList(array &$collection, $parent_id = null): void
