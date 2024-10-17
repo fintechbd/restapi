d2a534f2e6e51b8ccca0529d4de425a49f2df12e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-17 12:26:43 +0600

    break: deposit description column added

--- src/Http/Resources/Reload/DepositCollection.php
+++ src/Http/Resources/Reload/DepositCollection.php
@@ -21,6 +21,7 @@ class DepositCollection extends ResourceCollection
         return $this->collection->map(function ($deposit) {
             $data = [
                 'id' => $deposit->getKey(),
+                'description' => $deposit->description ?? null,
                 'source_country_id' => $deposit->source_country_id ?? null,
                 'source_country_name' => null,
                 'destination_country_id' => $deposit->destination_country_id ?? null,
