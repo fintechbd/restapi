f9d46cbe8a344e32604692f2f60494ffe92bd66e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-30 15:18:50 +0600

    feat: compliance response optimized

--- src/Http/Resources/Transaction/ComplianceCollection.php
+++ src/Http/Resources/Transaction/ComplianceCollection.php
@@ -16,7 +16,26 @@ class ComplianceCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collection->map(function ($compliance) {
+            return [
+                'id' => $compliance->getKey(),
+                'code' => $compliance->code ?? null,
+                'name' => $compliance->name ?? null,
+                'score' => $compliance->score ?? null,
+                'risk' => $compliance->risk ?? null,
+                'priority' => $compliance->priority ?? null,
+                'description' => $compliance->description ?? null,
+                'compliance_data' => $compliance->compliance_data ?? (object)[],
+                'user_name' => $compliance->user_name ?? null,
+                'user_mobile' => $compliance->user_mobile ?? null,
+                'order_source_country_name' => $compliance->source_country_name ?? null,
+                'order_destination_country_name' => $compliance->destination_country_name ?? null,
+                'order_number' => $compliance->order_number ?? null,
+                'order_status' => $compliance->order_status ?? null,
+                'created_at' => $compliance->created_at ?? null,
+                'updated_at' => $compliance->updated_at ?? null
+            ];
+        })->toArray();
     }
 
     /**
