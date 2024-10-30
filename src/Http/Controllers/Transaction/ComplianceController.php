72ed25df170c513bbf3ba472fb9cb34e1ed46347
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-30 17:27:48 +0600

    fix: compliance dropdown added

--- src/Http/Controllers/Transaction/ComplianceController.php
+++ src/Http/Controllers/Transaction/ComplianceController.php
@@ -71,7 +71,7 @@ class ComplianceController extends Controller
 
             $compliance = Transaction::compliance()->create($inputs);
 
-            if (! $compliance) {
+            if (!$compliance) {
                 throw (new StoreOperationException)->setModel(config('fintech.transaction.compliance_model'));
             }
 
@@ -100,7 +100,7 @@ class ComplianceController extends Controller
 
             $compliance = Transaction::compliance()->find($id);
 
-            if (! $compliance) {
+            if (!$compliance) {
                 throw (new ModelNotFoundException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
 
@@ -131,13 +131,13 @@ class ComplianceController extends Controller
 
             $compliance = Transaction::compliance()->find($id);
 
-            if (! $compliance) {
+            if (!$compliance) {
                 throw (new ModelNotFoundException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
 
             $inputs = $request->validated();
 
-            if (! Transaction::compliance()->update($id, $inputs)) {
+            if (!Transaction::compliance()->update($id, $inputs)) {
 
                 throw (new UpdateOperationException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
@@ -171,11 +171,11 @@ class ComplianceController extends Controller
 
             $compliance = Transaction::compliance()->find($id);
 
-            if (! $compliance) {
+            if (!$compliance) {
                 throw (new ModelNotFoundException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
 
-            if (! Transaction::compliance()->destroy($id)) {
+            if (!Transaction::compliance()->destroy($id)) {
 
                 throw (new DeleteOperationException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
@@ -207,11 +207,11 @@ class ComplianceController extends Controller
 
             $compliance = Transaction::compliance()->find($id, true);
 
-            if (! $compliance) {
+            if (!$compliance) {
                 throw (new ModelNotFoundException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
 
-            if (! Transaction::compliance()->restore($id)) {
+            if (!Transaction::compliance()->restore($id)) {
 
                 throw (new RestoreOperationException)->setModel(config('fintech.transaction.compliance_model'), $id);
             }
@@ -279,26 +279,73 @@ class ComplianceController extends Controller
      */
     public function dropdown(DropDownRequest $request): DropDownCollection|JsonResponse
     {
-        //        try {
-        $filters = $request->all();
+        try {
+            $filters = $request->all();
 
-        $polices = collect(get_declared_classes())->filter(function ($class) {
-            return Str::contains($class, 'Fintech');
-        });
+            $entries = collect([
+                [
+                    'label' => 'Large Cash Transaction',
+                    'attribute' => 'CP001'
+                ],
+                [
+                    'label' => 'Large Virtual Currency Transaction',
+                    'attribute' => 'CP002'
+                ],
+                [
+                    'label' => 'Electronic Funds Transfer',
+                    'attribute' => 'CP003'
+                ],
+                [
+                    'label' => 'Suspicious Transaction',
+                    'attribute' => 'CP004'
+                ],
+                [
+                    'label' => 'Client Due Diligence',
+                    'attribute' => 'CP005'
+                ],
+                [
+                    'label' => 'Structuring Detection',
+                    'attribute' => 'CP006'
+                ],
+                [
+                    'label' => 'High-Risk Countries',
+                    'attribute' => 'CP007'
+                ],
+                [
+                    'label' => 'HIO Detection',
+                    'attribute' => 'CP008'
+                ],
+                [
+                    'label' => 'PEP Detection',
+                    'attribute' => 'CP009'
+                ],
+                [
+                    'label' => 'Account Velocity',
+                    'attribute' => 'CP010'
+                ],
+                [
+                    'label' => 'New Product Usage',
+                    'attribute' => 'CP011'
+                ],
+                [
+                    'label' => 'Dormant Account Activity',
+                    'attribute' => 'CP012'
+                ],
+                [
+                    'label' => 'Third-Party Transactions',
+                    'attribute' => 'CP013'
+                ],
+                [
+                    'label' => 'Virtual Currency Travel Rule',
+                    'attribute' => 'CP014'
+                ],
 
-        dd($polices);
+            ]);
 
-        //            $entries = Auth::role()->list($filters)->map(function ($entry) use ($label, $attribute) {
-        //                return [
-        //                    'attribute' => $entry->{$attribute} ?? 'id',
-        //                    'label' => $entry->{$label} ?? 'name',
-        //                ];
-        //            })->toArray();
-        //
-        //            return new DropDownCollection($entries);
+            return new DropDownCollection($entries);
 
-        //        } catch (Exception $exception) {
-        //            return response()->failed($exception);
-        //        }
+        } catch (Exception $exception) {
+            return response()->failed($exception);
+        }
     }
 }
